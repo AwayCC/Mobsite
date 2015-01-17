3027443c93ecb3eeb2a866c87d6c61c38dec0898
Patrick Chen <b01901140@ntu.edu.tw>
2015-01-18 03:54:16 +0800

    manager update

--- app/src/main/assets/www/tool/js/managerRecord.js
+++ app/src/main/assets/www/tool/js/managerRecord.js
@@ -1,3 +1,29 @@
 /**
  * Created by patrickchen on 2015/1/18.
  */
+var manager = (manager) ? manager : {};
+if(!manager.config){
+   manager.config = {};
+   console.warn("managerRecord : 'manager.js' is not loaded");
+}
+manager.initRecord = function(){
+   var initObj = (function(){
+      var undoAction = function(act){
+
+      };
+      var redoAction = function(act){
+
+      };
+      var pushAction = function(act){
+
+      };
+      return {
+         undoAction: undoAction,
+         redoAction: redoAction,
+         pushAction: pushAction
+      };
+   })();
+   manager.undoAction = initObj.undoAction;
+   manager.redoAction = initObj.redoAction;
+   manager.pushAction = initObj.pushAction;
+};
